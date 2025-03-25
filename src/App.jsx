2a2675f22e62aac51e85efe330f9217c799a2d3d
fernandesnic <nicolasbafernandes@gmail.com>
2025-03-25 15:25:19 -0300

    api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,62 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Article } from "./components/Article/Article";
-import article1Img from "./assets/images/article1.png";
-import article2Img from "./assets/images/article2.png";
-import article3Img from "./assets/images/article3.png";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
 // Componente em classé é uma classe que herda a classe Component do React, e retorna HTML dentro do método render.
 
 // Componente funcional é uma função que retorna HTML
 
-class App extends React.Component {
+function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v4/articles/"
+      );
+      const newsData = response.data;
+      console.log(newsData);
+      setNews(newsData.results);
+    }
+
+    loadNews();
+  }, []);
   // Método responsável por renderizar o conteudo HTML do nosso componentes
-  render() {
-    return (
-      <>
-        <Navbar />
 
-        <section id="articles">
-          <Article
-            title="Designing Dashboards"
-            provider="NASA"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            similique aliquid repellendus magni, dicta, laudantium ullam quos
-            laborum perferendis necessitatibus officia? Quidem suscipit eveniet
-            amet perferendis repellendus harum sit nulla."
-            thumbnail={article1Img}
-          />
+  return (
+    <>
+      <Navbar />
 
-          <Article
-            title="Vibrant Portraits of 2020"
-            provider="SpaceNews"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            similique aliquid repellendus magni, dicta, laudantium ullam quos
-            laborum perferendis necessitatibus officia? Quidem suscipit eveniet
-            amet perferendis repellendus harum sit nulla."
-            thumbnail={article2Img}
+      <section id="articles">
+        {news.lenght === 0 ? (
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
           />
-
-          <Article
-            title="36 Days of Malayalam Type"
-            provider="Spaceflight Now"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            similique aliquid repellendus magni, dicta, laudantium ullam quos
-            laborum perferendis necessitatibus officia? Quidem suscipit eveniet
-            amet perferendis repellendus harum sit nulla."
-            thumbnail={article3Img}
-          />
-
-          <Article
-            title="Designing Dashboards"
-            provider="NASA"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            similique aliquid repellendus magni, dicta, laudantium ullam quos
-            laborum perferendis necessitatibus officia? Quidem suscipit eveniet
-            amet perferendis repellendus harum sit nulla."
-            thumbnail={article1Img}
-          />
-        </section>
-      </>
-    );
-  }
+        ) : (
+          news.map((article) => {
+            return (
+              <Article
+                key={article.id}
+                title={article.title}
+                provider={article.newsSite}
+                description={article.summary}
+                thumbnail={article.image_url}
+              />
+            );
+          })
+        )}
+      </section>
+    </>
+  );
 }
 
 export default App;
